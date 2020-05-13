9829517f8f2f88bfa4485ddf5c1241d44afa772e
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-05-13 14:12:08 -0300

    add page reset-password

--- src/routes/session.js
+++ src/routes/session.js
@@ -3,5 +3,6 @@ const routes = express.Router();
 
 routes.get('/login', (req, res) => res.render('session/login'));
 routes.get('/forgot-password', (req, res) => res.render('session/forgot-password'));
+routes.get('/reset-password', (req, res) => res.render('session/reset-password'));
 
 module.exports = routes;
