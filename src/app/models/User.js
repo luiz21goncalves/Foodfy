9d7ddc8cff8c984a4abb38c2b17f7e36ba08879a
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2020-05-21 10:03:55 -0300

    finished user create  validations

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,6 +1,24 @@
 const db = require('../../config/db');
 
 module.exports = {
+  all() {},
+
+  async findOne(filters) {
+    let query = `SELECT * FROM users`;
+
+    Object.keys(filters).map(key => {
+      query = `${query} ${key}`;
+
+      Object.keys(filters[key]).map(field => {
+        query = `${query} ${field} = '${filters[key][field]}'`;
+      });
+    });
+
+    const results = await db.query(query);
+
+    return results.rows[0];
+  },
+
   async create(data) {
     const query = `
       INSERT INTO  users (
@@ -21,6 +39,6 @@ module.exports = {
 
     const results = await db.query(query, values);
 
-    return results.rows[0]
+    return results.rows[0];
   },
 }
--- src/app/validators/user.js
+++ src/app/validators/user.js
@@ -1,4 +1,6 @@
-function post(req, res, next) {
+const User = require('../models/User');
+
+async function post(req, res, next) {
   const keys = Object.keys(req.body);
 
   for (key of keys) {
@@ -8,6 +10,14 @@ function post(req, res, next) {
         error: 'Apenas o campo de admistrador não é obrigatório.'
       });
   }
+
+  const user = await User.findOne({ where: { email: req.body.email } });
+
+  if (user)
+    return res.render('user/create', {
+      user: req.body,
+      error: 'Usuário  já cadastrado, <a href="/login">faça login</a> ou <a href="/forgot-password">recupere sua senha</a>.'
+    })
 
   next();
 };
